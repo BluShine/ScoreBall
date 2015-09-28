bad88c81506f43f03643fdfad2360d6f4a613c6c
BluShine <umbreon27@gmail.com>
2015-09-28 16:00:35 -0400

    Made ball prettier and added a script.
    Gave the ball a proper material and made a script for the ball.

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+    //handling
+    public float noTakebacksCooldown = .2f; //time in seconds
+
+    //private
+    Rigidbody body;
+    TeamPlayer currentPlayer;
+    TeamPlayer previousPlayer;
+    bool isHeld;
+
+	// Use this for initialization
+	void Start () {
+        body = GetComponent<Rigidbody>();
+	}
+	
+	// FixedUpdate is called at a fixed rate
+	void FixedUpdate () {
+        
+	}
+}
