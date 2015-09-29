27a09c0f1c01e2bb8f59e1e72a75b22772da0111
BluShine <umbreon27@gmail.com>
2015-09-28 21:56:35 -0400

    Added ball catch/shooting, and smoothed movement
    Added ball catching and shooting. Added spherecasts to the player for
    auto-correcting player movement while walking into walls. Added some
    layers to help with those spherecasts. Also created a "butterfingers"
    toggle that makes players drop balls if they run face-first into a wall.
    This prevents some slightly strange movement when you try swinging balls
    around next to walls.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -5,12 +5,14 @@ public class Ball : MonoBehaviour {
 
     //handling
     public float noTakebacksCooldown = .2f; //time in seconds
+    float takeTimer = 0;
+    public float carryRadius = .5f;
 
-    //private
+    //state
     Rigidbody body;
     TeamPlayer currentPlayer;
     TeamPlayer previousPlayer;
-    bool isHeld;
+    bool isHeld = false;
 
 	// Use this for initialization
 	void Start () {
@@ -19,6 +21,44 @@ public class Ball : MonoBehaviour {
 	
 	// FixedUpdate is called at a fixed rate
 	void FixedUpdate () {
-        
+        takeTimer -= Time.fixedDeltaTime;
+        takeTimer = Mathf.Max(0, takeTimer);
+        if (isHeld)
+        {
+            body.constraints = RigidbodyConstraints.FreezeRotation;
+        }
+        else
+        {
+            body.constraints = RigidbodyConstraints.None;
+        }
 	}
+
+    //try to grab the ball. Returns true if you got it.
+    public bool grabBall(TeamPlayer player)
+    {
+        if (takeTimer == 0)
+        {
+            Debug.Log("Grabbed!");
+            if (currentPlayer != null)
+            {
+                currentPlayer.removeBall(this);
+            }
+            takeTimer = noTakebacksCooldown;
+            previousPlayer = currentPlayer;
+            currentPlayer = player;
+            isHeld = true;
+            return true;
+        }
+        Debug.Log("Grab failed");
+        return false;
+    }
+
+    public void shoot(Vector3 shootVector)
+    {
+        isHeld = false;
+        body.constraints = RigidbodyConstraints.None;
+        takeTimer = noTakebacksCooldown;
+        body.velocity = shootVector;
+        currentPlayer.removeBall(this);
+    }
 }
--- Assets/Scripts/TeamPlayer.cs
+++ Assets/Scripts/TeamPlayer.cs
@@ -3,27 +3,38 @@ using System.Collections;
 
 public class TeamPlayer : MonoBehaviour {
 
-    public float moveSpeed = 10; //movement speed in pixels/sec
-    public float strafeSpeed = 5; //"strafing" speed in pixels/sec
+    //physics
+    public float moveSpeed = 500; //movement speed in pixels/sec
+    public float strafeSpeed = 500; //"strafing" speed in pixels/sec
     public float turnSpeed = 720; //rotation speed in pixels/sec
-
     [Range(0.01f, 0.99f)]
     public float strafeThreashold = .6f; //input threashold for strafing only. 
     [Range(0.01f, 0.99f)]
     public float turnThreashold = .3f; //input threashold for turning only.
+    //ball handling
+    public float ballHoldDistance = 1;
+    public float ballShootPower = 1000;
+    public bool butterFingers = false; //if true, player will drop the ball if we run face-first into a wall
 
+    //input
     public string xAxis = "Horizontal";
     public string yAxis = "Vertical";
+    public string shootButton = "Fire";
 
     Rigidbody body;
+
+    Ball carriedBall;
+
+    public LayerMask BALLMASK; 
 
 	// Use this for initialization
 	void Start () {
         body = GetComponent<Rigidbody>();
 	}
 	
-	// Update is called once per frame
+	// FixedUpdate is called at a fixed rate
 	void FixedUpdate () {
+        //MOVEMENT--------------------------------------------------------------------
         //get input vector from joystick/keys
         Vector2 input = new Vector2(Input.GetAxis(xAxis), Input.GetAxis(yAxis));
         if (input.magnitude > 1)
@@ -31,7 +42,6 @@ public class TeamPlayer : MonoBehaviour {
             input.Normalize();
         }
         float inputMag = input.magnitude;
-
         //rotate towards joystick
         if (inputMag != 0)
         {
@@ -44,15 +54,105 @@ public class TeamPlayer : MonoBehaviour {
         //strafing
         if (inputMag > turnThreashold)
         {
-            movingVel += Mathf.Min(1, inputMag / strafeThreashold) * strafeSpeed * Time.fixedDeltaTime * new Vector3(input.x, 0, input.y);
+            movingVel += Mathf.Min(1, inputMag / strafeThreashold) * strafeSpeed * new Vector3(input.x, 0, input.y);
         }
         //running
         if (inputMag > strafeThreashold)
         {
             Vector3 playerRot = transform.forward;
-            movingVel += inputMag * Time.fixedDeltaTime * moveSpeed * new Vector3(playerRot.x, 0, playerRot.z);
-            Debug.Log(playerRot);
+            movingVel += inputMag * moveSpeed * new Vector3(playerRot.x, 0, playerRot.z);
         }
+        //rotate our velocity if we're sliding along a wall.
+        Ray fRay = new Ray(transform.position, transform.forward);
+        RaycastHit fRHit;
+        //spherecast further if we're carrying a ball
+        float sDist = .5f;
+        if (carriedBall != null)
+        {
+            sDist += carriedBall.carryRadius + ballHoldDistance;
+        }
+        //Debug.DrawLine(transform.position, transform.position + transform.forward * sDist);
+        //spherecast forwards
+        if (Physics.SphereCast(fRay, .4f, out fRHit, sDist, BALLMASK))
+        {
+            Vector3 tDir = Vector3.Cross(fRHit.normal, transform.up);
+            if (Vector3.Dot(tDir, movingVel) > .5f)
+            {
+                movingVel = movingVel.magnitude * tDir;
+            }
+            else if (Vector3.Dot(-tDir, movingVel) > .5f)
+            {
+                movingVel = movingVel.magnitude * -tDir;
+            }
+        }
+        //Debug.DrawLine(transform.position, transform.position + body.velocity);
         body.velocity = movingVel;
+
+        //BALL HANDLING---------------------------------------------------------
+        if (carriedBall != null)
+        {
+            bool butterShot = false;
+            //stop and move away from walls if there's not enough space to hold the ball
+            Ray ballForwardRay = new Ray(transform.position, transform.forward);
+            RaycastHit ballForwardCast;
+            float rayDist = (ballHoldDistance + carriedBall.carryRadius) + .1f;
+            Debug.DrawLine(transform.position, transform.position + transform.forward * rayDist);
+            if (Physics.SphereCast(ballForwardRay, 0.4f, out ballForwardCast, rayDist, BALLMASK))
+            {
+                //stop
+                body.velocity = Vector3.zero;
+                //calculate the distance to move away from the wall
+                Debug.Log(Vector3.Dot(transform.forward, ballForwardCast.normal));
+                transform.position += ballForwardCast.normal * 
+                    (Vector3.Dot(transform.forward, ballForwardCast.normal) *
+                    (ballForwardCast.distance - rayDist)) * .5f;
+                if (butterFingers)
+                {
+                    butterShot = true;
+                }
+            }
+
+            //move the ball with us
+            carriedBall.transform.position = transform.position + transform.forward * ballHoldDistance;
+            //make sure that the ball is not getting stuck in the ground
+            RaycastHit ballDownCast;
+            Ray ballDownRay = new Ray(carriedBall.transform.position, Vector3.down);
+            if (Physics.Raycast(ballDownRay, out ballDownCast, ballHoldDistance * 2 + carriedBall.carryRadius * 2, BALLMASK))
+            {
+                carriedBall.transform.position = ballDownCast.point + new Vector3(0, carriedBall.carryRadius, 0);
+            }
+            //give it equal velocity to our velocity
+            carriedBall.GetComponent<Rigidbody>().velocity = body.GetPointVelocity(carriedBall.transform.position);
+            //shoot it
+            if (butterShot)
+            {
+                carriedBall.shoot((transform.forward + transform.up * .5f).normalized * ballShootPower * .5f);
+            }
+            else if (Input.GetButtonDown(shootButton))
+            {
+                carriedBall.shoot(transform.forward * ballShootPower);
+            }
+        }
 	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Ball collidedBall = collision.gameObject.GetComponent<Ball>();
+        if (collidedBall != null)
+        {
+            if (collidedBall.grabBall(this))
+            {
+                carriedBall = collidedBall;
+            }
+        }
+    }
+
+    public void removeBall(Ball rBall)
+    {
+        if (carriedBall == rBall)
+        {
+            carriedBall.transform.SetParent(null);
+            carriedBall = null;
+        }
+    }
 }
