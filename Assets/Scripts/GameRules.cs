005f75acb54a3de9b46d02ab84f8be8b09f66c72
Gregory Loden <gregory.loden@gmail.com>
2015-10-01 15:01:19 -0400

    add rule generation scene

--- Assets/Scripts/GameRules.cs
+++ Assets/Scripts/GameRules.cs
@@ -1,13 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class GameRules : MonoBehaviour {
+	GameRule rule = new GameRule();
+	public GameObject condition;
 
 	// Use this for initialization
-//	void Start () {
-//	
-//	}
+	void Start () {
+	}
 
 	// Update is called once per frame
 //	void Update () {
@@ -15,6 +17,34 @@ public class GameRules : MonoBehaviour {
 //	}
 
 	public void GenerateNewRule() {
-		Debug.Log("New rule generated");
+		Transform t = condition.transform;
+		rule.condition = new GameRuleCondition();
+		t.GetChild(0).gameObject.GetComponent<Text>().text = rule.condition.ToString();
+		rule.action = new GameRuleAction();
+		t.GetChild(1).gameObject.GetComponent<Text>().text = rule.action.ToString();
+//		outputText.text = rule.ToString();
+	}
+}
+
+public class GameRule {
+	public GameRuleCondition condition;
+	public GameRuleAction action;
+	public GameRule() {
+	}
+}
+
+public class GameRuleCondition {
+	public GameRuleCondition() {
+	}
+	public string ToString() {
+		return "If hello";
+	}
+}
+
+public class GameRuleAction {
+	public GameRuleAction() {
+	}
+	public string ToString() {
+		return "Then hello";
 	}
 }
