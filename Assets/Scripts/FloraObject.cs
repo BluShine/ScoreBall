72c18ee53c0f374f5933b7019f877b66a6f3bf5e
BluShine <umbreon27@gmail.com>
2015-11-23 17:03:34 -0500

    Added flora

---
+++ Assets/Scripts/FloraObject.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class FloraObject : MonoBehaviour {
+
+    public Gradient colorGradient;
+
+	// Use this for initialization
+	void Start () {
+        SpriteRenderer spr = GetComponent<SpriteRenderer>();
+        spr.color = colorGradient.Evaluate(Random.value);
+	}
+
+#if (UNITY_EDITOR)
+    // Update is called once per frame
+    void Update() {
+        if (!Application.isPlaying)
+        {
+            SpriteRenderer spr = GetComponent<SpriteRenderer>();
+            spr.color = colorGradient.Evaluate(Random.value);
+        }
+    }
+#endif
+}
