894288d781fd033b044a23b9b04e2791e47893ac
BluShine <umbreon27@gmail.com>
2015-12-14 14:16:25 -0500

    Added a restart button (press delete or f4). Also changed the lighting settings so that it looks right when you restart.

---
+++ Assets/Scripts/RestartButton.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RestartButton : MonoBehaviour {
+
+    public string button;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetButtonDown(button))
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+	}
+}
