2aa2765ca470afff75d99b7942d296b1230764ac
Mark <umbreon27@gmail.com>
2016-01-15 19:32:08 -0500

    Added twitch chat integration. Commands include "add rule", "delete rule", "add ball", "add fish", etc.

---
+++ Assets/TwitchChat/TwitchChatter.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class TwitchChatter : MonoBehaviour {
+
+    public GameRules gameRules;
+    public TwitchInterface twitchInterface;
+    public GameObject[] spawnables;
+    Dictionary<string, SpawnRef> spawnDict;
+    List<GameObject>[] spawnedObjects;
+    static int MAXOBJECTSOFTYPE = 20;
+
+    //struct to store a gameobject ref and it's index in 
+    struct SpawnRef {
+        public SpawnRef(GameObject gObj, int i) : this()
+        {
+            gameObj = gObj;
+            index = i;
+        }
+        public GameObject gameObj;
+        public int index;
+    }
+
+    void Start()
+    {
+        //build spawn dictionary
+        spawnDict = new Dictionary<string, SpawnRef>();
+        spawnedObjects = new List<GameObject>[spawnables.Length];
+        for(int i = 0; i < spawnables.Length; i++)
+        {
+            spawnDict.Add(spawnables[i].name, new SpawnRef(spawnables[i], i));
+            spawnedObjects[i] = new List<GameObject>(MAXOBJECTSOFTYPE);
+        }
+        //make sure we have gamerules and twitchinterface
+        if (gameRules == null)
+        {
+            gameRules = FindObjectOfType<GameRules>();
+        }
+        if(twitchInterface == null)
+        {
+            twitchInterface = FindObjectOfType<TwitchInterface>();
+        }
+        //set twitch interface event
+        twitchInterface.messageReciever += OnMessage;
+    } 
+
+	void OnMessage(string message)
+    {
+        if (message.Contains("add"))
+        {
+            if (message.Contains("rule"))
+            {
+                gameRules.GenerateNewRule();
+            }
+            else
+            {
+                string key = message.Remove(0, 4);
+                if (spawnDict.ContainsKey(key))
+                {
+                    SpawnRef spr = spawnDict[key];
+                    spawnedObjects[spr.index].Add(GameObject.Instantiate(spr.gameObj));
+                }
+            }
+        }
+        if(message.Contains("delete"))
+        {
+            if (message.Contains("rule"))
+            {
+                gameRules.deleteAllRules();
+            } else
+            {
+                string key = message.Remove(0, 7);
+                if (spawnDict.ContainsKey(key))
+                {
+                    SpawnRef spr = spawnDict[key];
+                    Debug.Log(spawnedObjects[spr.index].Count);
+                    if (spawnedObjects[spr.index].Count > 0)
+                    {
+                        GameObject.Destroy(spawnedObjects[spr.index][0]);
+                        spawnedObjects[spr.index].RemoveAt(0);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/TwitchChat/TwitchInterface.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net.Sockets;
+using System.IO;
+
+public class TwitchInterface : MonoBehaviour {
+
+    static string SERVER = "irc.twitch.tv";
+    static int PORT = 6667;
+
+    /// <summary>
+    /// twitch username
+    /// </summary>
+    public string username = "username";
+    /// <summary>
+    /// twitch oauth login token
+    /// go to https://twitchapps.com/tmi/ to generate a token
+    /// </summary>
+    public string oauthToken = "oauth:";
+    /// <summary>
+    /// channel to connect to: #username (all lowercase)
+    /// </summary>
+    public string channel = "#bobross";
+
+    //recieved messages delegate
+    public delegate void RecievedMessage(string message);
+    public RecievedMessage messageReciever;
+
+    private TcpClient tcpClient;
+    private NetworkStream nStream;
+    private StreamReader sReader;
+    private StreamWriter sWriter;
+
+    public struct TwitchChatMessage
+    {
+        public TwitchChatMessage(string msg, string send) : this()
+        {
+            message = msg;
+            sender = send;
+        }
+        public string message;
+        public string sender;
+    }
+
+    // Use this for initialization
+    void Start () {
+        Connect();
+	}
+
+    public void Connect()
+    {
+        tcpClient = new TcpClient(SERVER, PORT);
+        nStream = tcpClient.GetStream();
+        sReader = new StreamReader(nStream);
+        sWriter = new StreamWriter(nStream);
+
+        Write("USER " + username + "tmi twitch :" + username);
+        Write("PASS " + oauthToken);
+        Write("NICK " + username);
+    }
+
+    public void Write(string message)
+    {
+        sWriter.WriteLine(message);
+        sWriter.Flush();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //check for new data
+	    while(nStream.DataAvailable)
+        {
+            string data = sReader.ReadLine();
+            if (data != null)
+                ReadData(data);
+        }
+	}
+
+    void ReadData(string data)
+    {
+        string[] splitData = data.Split(' ');
+        switch(splitData[1])
+        {
+            case "PING":
+                //respond to pings
+                SendMessage("PONG " + splitData[2]);
+                break;
+            case "PRIVMSG":
+                messageReciever(data.Split(':')[2]);
+                break;
+            case "001":
+                Write(("MODE " + username + " +B"));
+                Write("JOIN " + channel);
+                Write("PRIVMSG " + channel + " :Game connected!");
+                break;
+            default:
+                break;
+        }
+    }
+}
