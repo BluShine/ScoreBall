97f44f8417ea0dfe03b4040af18b62883ce32df3
Mark T <umbreon27@gmail.com>
2016-01-25 06:10:28 -0500

    Created placeholder title screen and added quit buttons to main game and second-screen.

---
+++ Assets/Scripts/sceneswitcher.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class sceneswitcher : MonoBehaviour {
+
+	public void switchScene(int scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void quit()
+    {
+        Application.Quit();
+    }
+}
