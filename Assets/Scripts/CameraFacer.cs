d6d2368728dfce51ad0edbbcac90b8c35b4b18a6
Mark T <umbreon27@gmail.com>
2016-02-14 23:41:53 -0500

    Added camera-facing outlines to balls
    Added an animated outline to balls. Added a camera-facer script to make
    the outline rotate to face towards the camera

---
+++ Assets/Scripts/CameraFacer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFacer : MonoBehaviour {
+
+    Camera c;
+
+	// Use this for initialization
+	void Start () {
+        //save the camera reference
+        c = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //rotate ourself to face towards the camera.
+        transform.rotation = c.transform.rotation;
+	}
+}
