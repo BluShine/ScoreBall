60545a288abf1d4572cd388440f4836c1ee85f32
Mark T <umbreon27@gmail.com>
2016-03-03 04:32:41 -0500

    Puck prefab is named properly now.
    Puck prefab now has the correct name.

---
+++ Assets/EditorScripts/FBXImportAnnoyanceRemover.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+
+public class FBXImportAnnoyanceRemover : AssetPostprocessor
+{
+    void OnPreprocessModel()
+    {
+        ModelImporter importer = assetImporter as ModelImporter;
+        String name = importer.assetPath.ToLower();
+        if (name.Substring(name.Length - 4, 4) == ".fbx")
+        {
+            importer.globalScale = 50.0F;
+            importer.generateAnimations = ModelImporterGenerateAnimations.None;
+            importer.animationType = ModelImporterAnimationType.None;
+            importer.optimizeMesh = false;
+            importer.importBlendShapes = false;
+            importer.importMaterials = false;
+        }
+    }
+}
