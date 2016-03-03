51107c2376dac2085ab9b42c7735d2b8ced8b9f5
Mark T <umbreon27@gmail.com>
2016-03-03 03:40:05 -0500

    Sportsobjects are pushed back from the boundary
    Added a pushbackwoods, similar to throwback woods. It applies a very
    small amount of acceleration to sportsobjects that cross over the
    boundary lines.

---
+++ Assets/Scripts/PushBackWoods.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PushBackWoods : MonoBehaviour {
+
+    public Vector3 target = new Vector3(0, -100, 0); //target that sportsobjects will be launched towards
+    public float pushVelocity = 20f;
+
+    //if a sports object enters, shoot it back towards 
+    void OnTriggerEnter(Collider collision)
+    {
+        GameObject obj = collision.gameObject;
+        TeamPlayer tPlayer = obj.GetComponent<TeamPlayer>();
+        if(tPlayer != null)
+        {
+            return;
+        }
+        SportsObject sportsObj = obj.GetComponent<SportsObject>();
+        if(sportsObj != null)
+        {
+            sportsObj.body.AddForce((target - sportsObj.transform.position).normalized * pushVelocity, ForceMode.Acceleration);
+        }
+    }
+
+    void OnTriggerStay(Collider collision)
+    {
+        GameObject obj = collision.gameObject;
+        TeamPlayer tPlayer = obj.GetComponent<TeamPlayer>();
+        if (tPlayer != null)
+        {
+            return;
+        }
+        SportsObject sportsObj = obj.GetComponent<SportsObject>();
+        if (sportsObj != null)
+        {
+            sportsObj.body.AddForce((sportsObj.transform.position - target).normalized * pushVelocity, ForceMode.Acceleration);
+        }
+    }
+}
