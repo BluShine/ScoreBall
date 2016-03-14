1862b6d28d8262ef5c64200e2a3fa74a46ee45c7
Mark <umbreon27@gmail.com>
2016-03-14 13:02:48 -0400

    Hopefully fixed imports for real this time.
    Ok, I think this fixes the importing issue.

--- Assets/Editor/FBXImportAnnoyanceRemover.cs
+++ Assets/Editor/FBXImportAnnoyanceRemover.cs
@@ -8,9 +8,10 @@ public class FBXImportAnnoyanceRemover : AssetPostprocessor
     {
         ModelImporter importer = assetImporter as ModelImporter;
         String name = importer.assetPath.ToLower();
-        if (name.Substring(name.Length - 4, 4) == ".fbx" && importer.globalScale == 100.0f)
+        if (name.Substring(name.Length - 4, 4) == ".fbx" && importer.userData != "imported")
         {
-            importer.globalScale = 50.0F;
+            importer.userData = "imported";
+            //importer.globalScale = 50.0F;
             importer.generateAnimations = ModelImporterGenerateAnimations.None;
             importer.animationType = ModelImporterAnimationType.None;
             importer.optimizeMesh = false;
