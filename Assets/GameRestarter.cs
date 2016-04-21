e77dce4be6e124bc8bdf61c4ece1130738ae2556
Mark <umbreon27@gmail.com>
2016-04-21 18:59:57 -0400

    Can restart game on gameover screen.
    After the end of a match, you can press start/enter to restart the
    game.d

---
+++ Assets/GameRestarter.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameRestarter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(gameObject.activeSelf)
+        {
+            if(Input.GetButton("Submit"))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+	}
+}
